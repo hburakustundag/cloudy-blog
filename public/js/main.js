2994720e768bf9149bd35f75706fc977a82ccff8
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-11-11 17:49:03 +0300

    Add client-side js

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,17 @@
+const deleteBlog = document.querySelector(".delete-button");
 
+deleteBlog.addEventListener("click", (_) => {
+  fetch("/blogs", {
+    method: "delete",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      title: "Hello World!",
+    }),
+  })
+    .then((res) => {
+      if (res.ok) return res.json();
+    })
+    .then((response) => {
+      window.location.reload(true);
+    });
+});
