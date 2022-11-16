0a08d8f6c3283e3a785b4624f3c64db4dab69b23
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-11-16 18:07:52 +0300

    Delete blogsRouter.js

--- routes/blogsRouter.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const blogsController = require("../controllers/blogsController");
-
-router.get("/", blogsController.getBlogs);
-router.post("/createBlog", blogsController.createBlog);
-router.put("/addOneLike", blogsController.addOneLike);
-router.delete("/deleteBlog", blogsController.deleteBlog);
-
-module.exports = router;
