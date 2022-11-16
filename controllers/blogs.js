3fca4109e7e3f20057b761c529ea97f8b47d5cdd
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-11-16 18:06:28 +0300

    Create blogs.js

---
+++ controllers/blogs.js
@@ -0,0 +1,52 @@
+const Blog = require("../models/Blog");
+
+module.exports = {
+  getBlogs: async (req, res) => {
+    try {
+      const blogPosts = await Blog.find({ userId: req.user.id });
+      res.render("blogs.ejs", { blogs: blogPosts, user: req.user });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
+  createBlog: async (req, res) => {
+    try {
+      await Blog.create({
+        title: req.body.title,
+        blog: req.body.blog,
+        likes: 0,
+        userId: req.user.id,
+      });
+      console.log("Blog has been created");
+      res.redirect("/blogs");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  addOneLike: async (req, res) => {
+    try {
+      await Blog.findOneAndUpdate(
+        { _id: req.body.blogIdFromJSFile },
+        {
+          $inc: { likes: 1 },
+        }
+      );
+      console.log("Like Added");
+      res.json("Like Added");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  deleteBlog: async (req, res) => {
+    try {
+      await Blog.findOneAndDelete({ _id: req.body.blogIdFromJSFile });
+      console.log("Blog Deleted");
+      res.json("Blog Deleted");
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
