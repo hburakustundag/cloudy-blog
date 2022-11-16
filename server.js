681ad8ee2101e1abd7fcf81136dcd4fb21b51d5c
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-11-16 18:08:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
 require("dotenv").config({ path: "./config/.env" });
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("express-flash");
+const logger = require("morgan");
 const connectDB = require("./config/database");
-const homeRoute = require("./routes/homeRouter");
-const blogsRoute = require("./routes/blogsRouter");
+const mainRoutes = require("./routes/main");
+const blogsRoutes = require("./routes/blogs");
+const port = process.env.PORT || 8080;
 
-const port = 8080 || process.env.PORT;
-
+require("./config/passport")(passport);
 connectDB();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(logger("dev"));
+// Sessions
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
+  })
+);
 
-app.use("/", homeRoute);
-app.use("/blogs", blogsRoute);
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+app.use("/", mainRoutes);
+app.use("/blogs", blogsRoutes);
 
 app.listen(port, () => console.log(`App is listening on port ${port}`));
