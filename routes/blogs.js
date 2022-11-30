bdf42951cb5e09e0b2a5a8c22a30f23de3334762
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-11-30 22:15:56 +0300

    Create new route with params

--- routes/blogs.js
+++ routes/blogs.js
@@ -3,6 +3,7 @@ const blogsController = require("../controllers/blogs");
 const { ensureAuth } = require("../middleware/auth");
 
 router.get("/", ensureAuth, blogsController.getBlogs);
+router.get("/:id", blogsController.getSampleBlogs);
 router.post("/", blogsController.createBlog);
 router.put("/", blogsController.addOneLike);
 router.delete("/", blogsController.deleteBlog);
