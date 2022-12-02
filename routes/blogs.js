7d7670a7446ec57d4febdad0210624874df7bae0
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-12-02 16:50:41 +0300

    Update blogs.js

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const blogsController = require("../controllers/blogs");
+const upload = require("../middleware/multer");
 const { ensureAuth } = require("../middleware/auth");
 
 router.get("/", ensureAuth, blogsController.getBlogs);
 router.get("/:id", blogsController.getSampleBlogs);
-router.post("/", blogsController.createBlog);
+router.post("/", upload.single("file"), blogsController.createBlog);
 router.put("/", blogsController.addOneLike);
 router.delete("/", blogsController.deleteBlog);
 
