d2c01c99a2488f8ba568e3440e0cf9df94894989
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-12-02 16:50:16 +0300

    Create Comment.js

---
+++ models/Comment.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  blogId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Blog",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
