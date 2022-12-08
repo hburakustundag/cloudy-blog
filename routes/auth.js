9ebc0b16004ee29629363761770e63fb67976a2b
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-12-08 20:33:20 +0300

    Add facebook oauth routes

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,28 @@ const passport = require("passport");
 require("dotenv").config({ path: "../config/.env" });
 
 router.get(
-  "/",
+  "/google",
   passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
 router.get(
   "/google/callback",
   passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    res.redirect("/blogs");
+  }
+);
+
+router.get(
+  "/facebook",
+  passport.authenticate("facebook", {
+    scope: ["id", "displayName", "photos", "email"],
+  })
+);
+
+router.get(
+  "/facebook/callback",
+  passport.authenticate("facebook", { failureRedirect: "/login" }),
   function (req, res) {
     res.redirect("/blogs");
   }
