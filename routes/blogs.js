3c49d1842ce8b48ba3f145b898d2e1e20b9a7a07
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-12-08 11:48:50 +0300

    Create new router for new blog entry

--- routes/blogs.js
+++ routes/blogs.js
@@ -4,6 +4,7 @@ const upload = require("../middleware/multer");
 const { ensureAuth } = require("../middleware/auth");
 
 router.get("/", ensureAuth, blogsController.getBlogs);
+router.get("/new", blogsController.getNewBlog);
 router.get("/:id", blogsController.getSampleBlogs);
 router.post("/", upload.single("file"), blogsController.createBlog);
 router.put("/", blogsController.addOneLike);
