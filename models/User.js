5c60e636887b1e17b64ca9871a660836f9780b94
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-12-08 18:11:49 +0300

    Update user schema structure

--- models/User.js
+++ models/User.js
@@ -2,9 +2,11 @@ const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, unique: true },
-  email: { type: String, unique: true },
+  username: { type: String, unique: true, sparse: true },
+  email: { type: String },
   password: String,
+  hasPassword: { type: Boolean, default: true },
+  googleId: String,
 });
 
 // Password hash middleware.
