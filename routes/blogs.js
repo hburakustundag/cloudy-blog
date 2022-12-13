9e1c7b2d536cfcefe3e1f89ecc1a2b07f5616e72
Halit Burak Ustundag <hburakustundag@gmail.com>
2022-12-13 13:50:03 +0300

    Add post and delete routes for comments

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const blogsController = require("../controllers/blogs");
 const upload = require("../middleware/multer");
+const commentController = require("../controllers/comment");
 const { ensureAuth } = require("../middleware/auth");
 
 router.get("/", blogsController.getBlogs);
@@ -9,5 +10,8 @@ router.get("/:id", blogsController.getOneBlog);
 router.post("/", upload.single("file"), blogsController.createBlog);
 router.put("/:id", blogsController.likePost);
 router.delete("/:id", blogsController.deletePost);
+
+router.post("/:id/comment", ensureAuth, commentController.createComment);
+router.delete("/:id/:commentId", commentController.deleteComment);
 
 module.exports = router;
